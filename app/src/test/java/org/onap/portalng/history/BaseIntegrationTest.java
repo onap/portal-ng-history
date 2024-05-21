3657268ba54eb250a74d7f86b592e995ed0488c1
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2024-05-21 11:01:32 +0200

    Adjust keycloak url after KeycloakX upgrade
    Issue-ID: PORTALNG-104
    Change-Id: Ia58100883bb1d23c2f61d3f8d7ce3bfc37358135
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>

--- app/src/test/java/org/onap/portalng/history/BaseIntegrationTest.java
+++ app/src/test/java/org/onap/portalng/history/BaseIntegrationTest.java
@@ -78,7 +78,7 @@ public abstract class BaseIntegrationTest {
     WireMock.stubFor(
         WireMock.get(
                 WireMock.urlMatching(
-                    String.format("/auth/realms/%s/protocol/openid-connect/certs", realm)))
+                    String.format("/realms/%s/protocol/openid-connect/certs", realm)))
             .willReturn(
                 WireMock.aResponse()
                     .withHeader("Content-Type", JWKSet.MIME_TYPE)
@@ -90,7 +90,7 @@ public abstract class BaseIntegrationTest {
     WireMock.stubFor(
         WireMock.post(
                 WireMock.urlMatching(
-                    String.format("/auth/realms/%s/protocol/openid-connect/token", realm)))
+                    String.format("/realms/%s/protocol/openid-connect/token", realm)))
             .withBasicAuth("test", "test")
             .withRequestBody(WireMock.containing("grant_type=client_credentials"))
             .willReturn(
